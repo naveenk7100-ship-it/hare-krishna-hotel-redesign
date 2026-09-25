6779293ca73c9b7127869f37ee65b641a79b22c6
Naveen <naveenk7100@gmail.com>
2026-09-25 11:15:14 +0530

    Configure GitHub Pages deployment via GitHub Actions

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/hare-krishna-hotel-redesign/',
   plugins: [react()],
   server: {
     port: 3000,
